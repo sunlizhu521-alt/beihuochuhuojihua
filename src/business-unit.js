f6512e6454401a46bd7fa4bb68bbcc95c1514ec4
擎天柱 <qingtianzhu@qingtianzhudeMacBook-Pro.local>
2026-09-02 11:21:44 +0800

    refactor: 删除 App.jsx 内联死组件

--- src/business-unit.js
+++
@@ -1,7 +0,0 @@
-export function purchaseTrackingBusinessUnit(value) {
-  const normalizedUnits = String(value ?? '')
-    .split(/[、,，+]/)
-    .map((unit) => unit.trim().split(/[*\uff0a]/, 1)[0].trim())
-    .filter(Boolean);
-  return [...new Set(normalizedUnits)].join('、');
-}
